8ab3e43736394694bc078f0bed9fc5a27b1b287e
Balakrishnan. P <433607@conneqtcorp.com>
2024-10-30 01:26:16 +0530

    day5spreadsheet update

--- blocks/day5spreadsheet/day5spreadsheet.js
+++ blocks/day5spreadsheet/day5spreadsheet.js
@@ -1,6 +1,26 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
+
+export async function fetchData(source) {
+    const response = await fetch(source);
+    if (!response.ok) {
+      // eslint-disable-next-line no-console
+      console.error('error loading API response', response);
+      return null;
+    }
+  
+    const json = await response.json();
+    if (!json) {
+      // eslint-disable-next-line no-console
+      console.error('empty API response', source);
+      return null;
+    }
+  
+    return json.data;
+  }
+  
+
 /**
  * loads and decorates the json
  * @param {Element} block The block element
@@ -15,5 +35,17 @@ export default async function decorate(block) {
   console.log(source);
   block.innerHTML = '';
 
-  block.append("<div>list</div>");
+  const resp = await fetch(`${source}`);
+  const json = await resp.json();
+  console.log(json);
+
+  const ul = document.createElement('ul');
+  json.data.forEach((row) => {
+    console.log(row.Source);
+    const li = document.createElement('li');
+    li.textContent=row.Source;
+    ul.append(li);
+  });
+
+  block.append(ul);
 }
